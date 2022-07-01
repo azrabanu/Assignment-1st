40f3e300e249e3f4da0f97c72ecef24974ae0bd6
azrabanu <100359301+azrabanu@users.noreply.github.com>
2022-07-01 19:50:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const obj = {
+    fname:"Atul Kumar",
+    lname : "Pandey"
+}
+const server = http.createServer(function(req,res){
+    res.write("<h1>this is our first server</h1>")
+    res.write("This is a normal text")
+    res.write(JSON.stringify(obj))
+    res.write(JSON.stringify({name:"rahul"}))
+    res.write("<h2>JSON.stringify(obj)</h2>")
+    res.end("<h1>This is the end of the server</h1>");
+    
+})
+
+
+server.listen(4000)
